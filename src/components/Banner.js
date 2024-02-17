21ab23d75cde49dabfca494f40ce64ebae09c287
abdelrahman ali <aabdelrhman0555@gmail.com>
2024-02-17 23:06:49 +0200

    oooo

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -58,7 +58,7 @@ export const Banner = () => {
                   <span className="tagline">Welcome to my Portfolio</span>
                   <h1>{`Hi! I'm Abdelrahman`} <span className="txt-rotate" data-period="1000" data-rotate='["Web Developer", "Web Designer", "UI/UX Designer"]'><span className="wrap">{text}</span></span></h1>
                   <p>Hi I am Abdelrahman Ali , I am a web developer , logo Designer and UX/UI Designer</p>
-                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+                  <button onClick={() => console.log('connect')}>Let's Connect <ArrowRightCircle size={25} /></button>
                 </div>}
             </TrackVisibility>
           </Col>
